b181bf48d5d0b958d3c1389df9c55f3ef9a15a75
OceanNawshad <reshad947@gmail.com>
2018-06-07 12:25:04 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+
+	
+<?php
+	
+	echo "<h1><a href='https://www.google.com/'>Google Search Engine</a></h1>";
+	$arraynumber = array("One", "Two", "Three","Four","Five","Six","Seven","Eight","Nine","Ten");
+	for($i=1;$i<11;$i++)
+	{
+		echo "<marquee><h1 style='color:DodgerBlue;'>Nawshad Noor Ocean</h1></marquee>";
+		
+	}
+	for($i=1;$i<11;$i++)
+	{
+		echo "<marquee><h1>$i. Nawshad Noor Ocean</h1></marquee>";
+		
+	}
+	for($i=0;$i<10;$i++)
+	{
+		echo "<marquee><h1>$arraynumber[$i]. Nawshad Noor Ocean</h1></marquee>";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+?>
